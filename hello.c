e9b8e17de1e5d02a6bc3b7289ab2ec61204ed30e
TreysenCraig <46788503+TreysenCraig@users.noreply.github.com>
2019-01-17 09:55:51 -0600

    Update hello.c

--- hello.c
+++ hello.c
@@ -5,6 +5,8 @@ int main(int argc, char **argv){
 
   printf("Sam Yunker/n");
   printf("Electrical Engineering/n");
-
+  printf("Treysen Craig/n");
+  printf("Electrical Engineering/n");
+  
   return(0);
 }
